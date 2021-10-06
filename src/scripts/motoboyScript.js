a75c4ddde3bc42dc14b1924522e734f7e74ab9d2
Daniel <dsouzaborini@gmail.com>
2021-10-06 04:58:05 -0300

    finaliza funçoes e verifica resultado

--- src/scripts/motoboyScript.js
+++ src/scripts/motoboyScript.js
@@ -3,25 +3,28 @@ import { stores } from "../data/storeData.js";
 
 const missingInfoMessage = "Informações ausentes. É necessário informar um motoboy.";
 const invalidInfoMessage = `Informações invalidas. É necessário informar um motoboy que existe, no formato "Moto <numero>"`;
+// const sucessMessage;
 
 const percentage = (percentage, totalValue) => {
   return (percentage * totalValue) / 100;
-} 
+}; 
 
 const getMotoboyResults = ({ name, deliveryPrice, stores }, storesData) => {
   let allOrders = [];
   let amountReceived = 0;
-  const numberOfOrders = allOrders.length;
-
   
   const orders = stores.forEach((stores) => {
-    const selectedStoreOrders = (storesData.find((storeInfo) => storeInfo.name === stores).orders);
-    selectedStoreOrders.forEach((price) => allOrders.push(price));
+    const selectedStore = (storesData.find((storeInfo) => storeInfo.name === stores));
+    selectedStore.orders.forEach((price) => allOrders.push(price));
+    const values = selectedStore.orders.reduce((total, number) => total + number, 0);
+    amountReceived = amountReceived + percentage(selectedStore.payment, values);
   });
 
+  const numberOfOrders = allOrders.length;
+  let fixedValue = deliveryPrice * numberOfOrders;
+  const totalValue = amountReceived + fixedValue;
 
-  
-  console.log(allOrders);
+  console.log(`${name} possui um total de ${numberOfOrders} pedidos, das lojas: ${stores}. Receberá no total o valor de: R$ ${totalValue}.`);
 };
 
 const getMotoboyInfo = (motoboySelected) => {
@@ -31,4 +34,5 @@ const getMotoboyInfo = (motoboySelected) => {
   return invalidInfoMessage;
 };
 
-getMotoboyInfo('Moto 4');
+getMotoboyInfo('Moto 3');
+// console.log(percentage(5, 550));
