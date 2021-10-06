ee535d22c4c67f7d712faca750b642d868e487df
Daniel <dsouzaborini@gmail.com>
2021-10-06 02:24:13 -0300

    first commit, add motoboys data file

---
+++ src/data/motoboysData.js
@@ -0,0 +1,28 @@
+const motoboys = [
+  {
+    "name": "deliveryman1",
+    "deliveryPrice": 2,
+    "stores": [1, 2, 3],
+  },
+  {
+    "name": "deliveryman2",
+    "deliveryPrice": 2,
+    "stores": [1, 2, 3],
+  },
+  {
+    "name": "deliveryman3",
+    "deliveryPrice": 2,
+    "stores": [1, 2, 3],
+  },
+  {
+    "name": "deliveryman4",
+    "deliveryPrice": 2,
+    "stores": [1],
+  },
+  {
+    "name": "deliveryman5",
+    "deliveryPrice": 3,
+    "stores": [1, 2, 3],
+  }
+];
+  
